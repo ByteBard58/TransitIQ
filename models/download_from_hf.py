ffad80463fa611c5de4f6d403aa9e9fc764734d1
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-04-13 01:44:01 +0600

    Initialize module-like structure in `download_from_hf.py`

--- models/download_from_hf.py
+++ models/download_from_hf.py
@@ -1,18 +1,22 @@
 from huggingface_hub import hf_hub_download as download_hf
 import pickle
 
+def download() -> None:
 # Download the serialized pipeline 
-download_hf(
-  repo_id="ByteBard101/TransitIQ",
-  filename="models/pipe.pkl",
-  repo_type="model",
-  local_dir="."
-)
+  download_hf(
+    repo_id="ByteBard101/TransitIQ",
+    filename="models/pipe.pkl",
+    repo_type="model",
+    local_dir="."
+  )
 
-# Download the serialized column names file
-download_hf(
-  repo_id="ByteBard101/TransitIQ",
-  filename="models/column_names.pkl",
-  repo_type="model",
-  local_dir="."
-)
+  # Download the serialized column names file
+  download_hf(
+    repo_id="ByteBard101/TransitIQ",
+    filename="models/column_names.pkl",
+    repo_type="model",
+    local_dir="."
+  )
+
+if __name__ == "__main__":
+  download()
