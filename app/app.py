d77f204686fe4ca2d5d33a703e954d41ef366ec1
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-04-13 15:57:51 +0600

    Add `/about`,`/predict` and `/predict/batch` endpoints for both regular and batch prediction to `app.py`

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, UploadFile
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.exceptions import HTTPException
+from fastapi.staticfiles import StaticFiles
 from .schema.validate import UserInput
 from models.download_from_hf import download
+from sklearn.pipeline import Pipeline
+from typing import Tuple, List
 
+import joblib
 import pandas as pd
+import numpy as np
+from contextlib import asynccontextmanager
 import os
 from pathlib import Path
 
@@ -10,10 +19,12 @@ from pathlib import Path
 MODEL_DIR = "models"
 PIPE_PATH = os.path.join(MODEL_DIR, "pipe.pkl")
 COLUMNS_PATH = os.path.join(MODEL_DIR, "column_names.pkl")
+INDEX_PATH = Path("app","templates","index.html")
+ABOUT_PATH = Path("app","templates","about.html")
 reverse_mapping = {0: "FALSE POSITIVE", 1: "CANDIDATE", 2: "CONFIRMED"}
 
 # --- Self-Heal Function ---
-def initialize_artifacts():
+def initialize_artifacts() -> Tuple[Pipeline,np.ndarray]:
     """
     Checks if model artifacts exist. If not, runs the training script.
     """
@@ -43,3 +54,137 @@ def initialize_artifacts():
             exit(1) # Exit if training fails
     else:
         print("Model artifacts found. Loading...")
+        pipe = joblib.load(PIPE_PATH)
+        column_names = joblib.load(COLUMNS_PATH)
+        print("Model artifacts are loaded. Ready for prediction 🚀")
+        return pipe,column_names
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    """
+    Loads the models at start
+    """
+    pipe,column_names = initialize_artifacts()
+
+    app.state.pipe = pipe
+    app.state.column_names = column_names
+
+    yield
+
+app = FastAPI(title="TransitIQ",version="3.0 (ByteBard58_Fork-FastAPI)",lifespan=lifespan)
+
+# Mount static files
+app.mount(name="static",path="/static",app=StaticFiles(
+    directory=Path("app","static")
+))
+
+async def get_artifacts(request:Request) -> Tuple[Pipeline,np.ndarray]:
+    """
+    Helper to serve the artifacts in a route
+    """
+    return request.app.state.pipe, request.app.state.column_names
+
+def validate_csv(target:pd.DataFrame,expected_columns:List) -> pd.DataFrame:
+    """
+    Helper for validating user-uploaded `.csv` files during batch prediction
+    """
+    if target.columns != expected_columns:
+        raise HTTPException(
+            status_code=422,
+            detail="The columns of the uploaded `.csv` files do not match with the expected list of columns or the order of them"
+
+        )
+    try:
+        target.astype(float)
+    except Exception:
+        raise HTTPException(
+            status_code=422,
+            detail = "Provided values must be numeric (float-compatible)"
+        )
+    
+    return target
+
+@app.get("/health")
+def health():
+    msg = {
+        "title":"TransitIQ",
+        "version":"3.0(ByteBard58_Fork-FastAPI)",
+        "status":"All systems operational"
+    }
+    return JSONResponse(content=msg,status_code=200)
+
+@app.get("/")
+def home():
+    msg = "Welcome to TransitIQ !"
+    return JSONResponse(content=msg,status_code=200)
+
+@app.get("/about")
+def about():
+    return FileResponse(ABOUT_PATH,status_code=200)
+
+reverse_mapping = {0: "FALSE POSITIVE", 1: "CANDIDATE", 2: "CONFIRMED"}
+
+@app.post("/predict")
+def predict_with_manual_inputs(
+    payload:UserInput, 
+    dep:Tuple[Pipeline,np.ndarray] = Depends(get_artifacts)
+):
+    pipe, column_names = dep
+    column_names:List = column_names.tolist()
+    payload:dict = payload.model_dump(mode="json")
+
+    sample = []
+    for i,(key,val) in enumerate(payload.items()):
+        if column_names[i] == key:
+            sample.append(val)
+        else:
+            raise ValueError("The payload does not match the original order defined by the BaseModel")
+    sample = np.array(sample).reshape(1,-1)
+    
+    label = int(pipe.predict(sample)[0])
+    proba:List = pipe.predict_proba(sample)[0].tolist()
+
+    label:str = reverse_mapping.get(label)
+    proba:dict = {cls:round(proba,3) for cls,proba in zip(reverse_mapping.values(),proba)}
+    msg = {
+        "status":"prediction successful",
+        "predicted_label":label,
+        "predction_probability":proba
+    }
+
+    return JSONResponse(status_code=201,content=msg)
+
+@app.post("/predict/batch")
+def predict_with_batch_input(
+    file:UploadFile, 
+    dep:Tuple[Pipeline,np.ndarray] = Depends(get_artifacts)
+):
+    pipe, column_names = dep
+    column_names:List = column_names.tolist()
+
+    ext = Path(file.filename).suffix
+    if ext != ".csv":
+        raise HTTPException(
+            status_code=422, detail=f"Only `.csv` file is allowed as an upload, got {ext} instead"
+        )
+    
+    try:
+        df = pd.read_csv(file)
+    except Exception as e:
+        raise HTTPException(status_code=422, detail=f"Failed to parse CSV file tracking: {str(e)}")
+    df:np.ndarray = validate_csv(df).to_numpy()
+
+    sample = df
+    label:List[float] = pipe.predict(sample).tolist()
+    proba:List[List[float]] = pipe.predict_proba(sample).tolist()
+
+    label:List[str] = [reverse_mapping.get(l) for l in label]
+    proba = [[round(value,3) for value in prob] for prob in proba]
+    proba
+    msg = {
+        "status":"batch prediction successful",
+        "predicted_labels":label,
+        "predction_probability":proba
+    }
+
+    return JSONResponse(status_code=201,content=msg)
